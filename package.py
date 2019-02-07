840ebc6561f9fe33a63de3c922125137f675228b
FeyditRémi <remi.feydit@ynov.com>
2019-02-07 18:36:21 +0100

    Changement non variables

--- package.py
+++ package.py
@@ -14,15 +14,15 @@ arg = parser.parse_args()
 def convert():
     for md_file in os.listdir(arg.input_directory):
         with open(arg.input_directory + '/' + md_file, 'r+') as fileToConvert:
-            test = fileToConvert.read()
-            test = test.replace('  ##', '##')
-            print(test)
-            html_file = open(arg.output_directory + '/' + md_file.replace('.md', '.html'), 'w')
-            link_patterns=[(re.compile(r'((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+(:[0-9]+)?|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)'),r'\1')]
-            link = Markdown(extras=["link-patterns", "cuddled-lists", "target-blank-links", "xml"],link_patterns=link_patterns)
-            convert_html = link.convert(test)
-            Markdown(convert_html)
-            html_file.write(convert_html)
+            ajustedFile = fileToConvert.read()
+            ajustedFile = ajustedFile.replace('  ##', '##')
+            print(ajustedFile)
+            htmlFile = open(arg.output_directory + '/' + md_file.replace('.md', '.html'), 'w')
+            linkPatterns=[(re.compile(r'((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+(:[0-9]+)?|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)'),r'\1')]
+            link = Markdown(extras=["link-patterns", "cuddled-lists", "target-blank-links", "xml"],link_patterns=linkPatterns)
+            convertHTML = link.convert(ajustedFile)
+            Markdown(convertHTML)
+            htmlFile.write(convertHTML)
     print("ouais super c'est fini")
 
 if arg.input_directory == None or arg.output_directory == None :
