f86453d87602bc4f448adf015fad5489ee0a0cf2
FeyditRémi <remi.feydit@ynov.com>
2019-02-11 18:56:52 +0100

    modification template et écriture README.md

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-MarkD_To_HTML
+# Markdown to HTML
+
+## Présentation du projet
+
+Le programme a pour but de de convertir un dossier contenant des fichiers markdown (avec l'extension `.md`) en fichiers HTML (avec l'extension `.html`) grâce à un programme en Python.
+
+Pour cela j'ai utilisé un package pour faciliter le programme (markdown2).
+
+### Avant de convertir
+
+Avant de pouvoir convertir vos fichiers markdown vous devez d'abord télécharger le package `markdown2`.
+
+Pour cela rentrer dans votre Powershell:
+```bash
+pip install -r requirements.txt
+```
+----
+### Commande et arguments
+
+Pour utiliser le convertisseur, mettez vos fichiers à convertir dans le fichier `markdown_file`, puis dans votre PowerShell exécutez le programme `markdownToHTML.py`.
+Pour connaître les arguments nécessaires pour convertir tapez:
+```bash
+python markdownToHTML.py -h
+```
+OU
+```bash
+python markdown.py --help
+```
+
+On appelle ensuite le programme avec `markdownToHTML.py`.
+
+Voici les arguments que l'on peut passer dans notre commande:
+
+Argument version courte | Argument version longue | Explications
+------------ | ------------- |-----------
+-h | --help | Affiche tous les arguments possibles et vous explique comment les utiliser
+-i | --input-directory | Dossier contenant les fichiers Markdown
+-o | --output-directory | Dossier destination pour les fichiers HTML
+-t | --template-directory | Fichier template pour votre HTML converti
+
+### Les différents types de templates
+
+`template1.html`:
+* Texte converti en rouge et avec la police 'Courier New'
+* Titre en h1 en vert pomme et titre aligné
+
+`template2.html`:
+* Lien en violet et les liens ne sont pas souligné
+* Fond en bleu ciel
+* Aligne les listes au centre
+
+`template3.html`:
+* Texte converti en blanc et fond en noir
+* Lien en blanc et les liens ne sont pas soulignés
+
+`template4.html`:
+* Titre h1 en rouge
+* Titre h2 en vert
+* Titre h3 en bleu
+* Lien en violet et les liens ne sont pas soulignés
+* Paragraphe en jaune
+* Fond en rose saumon
--- markdownToHTML.py
+++ markdownToHTML.py
@@ -4,27 +4,30 @@ from re import compile
 from os import listdir
 
 parser = ArgumentParser()
-parser.add_argument("-i", "--input-directory", help="The folder you want to convert", type = str)
-parser.add_argument("-o", "--output-directory", help="The name of HTML folder", type = str)
-parser.add_argument("-t", "--template-directory", help="The name of your template file", type = str)
+parser.add_argument("-i", "--input-directory", metavar = '', help="The folder you want to convert", type = str)
+parser.add_argument("-o", "--output-directory", metavar = '', help="The name of HTML folder", type = str)
+parser.add_argument("-t", "--template-directory", metavar = '', help="The name of your template file", type = str)
 arg = parser.parse_args()
 
 
 def convert():
     for md_file in listdir(arg.input_directory):
-        with open(arg.input_directory + '/' + md_file, 'r+') as fileToConvert:
-            template = open('template/' + arg.template_directory , 'r+')
-            adjustedFile = fileToConvert.read()
-            adjustedFile = adjustedFile.replace('  ##', '##')
-            htmlFile = open(arg.output_directory + '/' + md_file.replace('.md', '.html'), 'w')
-            linkPatterns=[(compile(r'((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+(:[0-9]+)?|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)'),r'\1')]
-            link = Markdown(extras=["link-patterns", "cuddled-lists", "target-blank-links", "xml"],link_patterns=linkPatterns)
-            convertHTML = link.convert(adjustedFile)
-            templates = template.read()
-            templates = templates.replace('replace', convertHTML)
-            templates = templates.replace('Page_Title', md_file.replace('.md', ''))
-            htmlFile.write(templates)
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nVotre conversion est terminée\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        if not md_file.endswith('.md'):
+            print('Mauvaise extensions de fichier')
+        else:
+            with open(arg.input_directory + '/' + md_file, 'r+') as fileToConvert:
+                template = open('template/' + arg.template_directory , 'r+')
+                adjustedFile = fileToConvert.read()
+                adjustedFile = adjustedFile.replace('  ##', '##')
+                htmlFile = open(arg.output_directory + '/' + md_file.replace('.md', '.html'), 'w')
+                linkPatterns=[(compile(r'((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+(:[0-9]+)?|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)'),r'\1')]
+                link = Markdown(extras=["link-patterns", "cuddled-lists", "target-blank-links", "xml"],link_patterns=linkPatterns)
+                convertHTML = link.convert(adjustedFile)
+                templates = template.read()
+                templates = templates.replace('replace', convertHTML)
+                templates = templates.replace('Page_Title', md_file.replace('.md', ''))
+                htmlFile.write(templates)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nVotre conversion est terminée\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
 if arg.input_directory == None or arg.output_directory == None or arg.template_directory == None:
